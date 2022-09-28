278cc7f2524ffcf18a6842e0279996014a367737
Abhishek <abhishek4080@gmail.com>
2022-09-28 21:01:07 +0530

    Added more comments to question2.py

--- Question2.py
+++ Question2.py
@@ -3,13 +3,13 @@ test_cases = int(input(""))
 
 for i in range(test_cases):
     count = 0
-    N,K = map(int,input().split())
+    N,K = map(int,input().split())  
     # N ==> number of players between Gi-Hun and Ali
     # K ==> height of both of them respectively.
-    H = list(map(int,input().split()))
+    H = list(map(int,input().split())) # applying int function for each item in input()
     # space-separated list of integers,denoting the heights of the players between Gi-Hun and Ali.
-    for j in range(N):
-        if H[j]>K:
+    for j in range(N): # nested for loop to compare elements
+        if H[j]>K:  # comparing all elements in the list (one by one) with height K
             count+=1
     print(count)        
 
